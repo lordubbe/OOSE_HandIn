2b083cf0a647032368a40ff922e51999c0671145
mihai9323 <mihai9323@gmail.com>
2014-03-26 19:37:57 +0100

    Added transparent walls
    The walls become transparent when the player is behind them

---
+++ Assets/Mihai/Scripts/TransparentWall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentWall : MonoBehaviour {
+	public Material transparentMat;
+	public float revertTime = 5f;
+	
+	private Material initialMat;
+	// Use this for initialization
+	void Start () {
+		initialMat = gameObject.renderer.material;
+		InvokeRepeating("revertMat",revertTime,revertTime);
+	}
+	
+	
+	void revertMat(){
+		if(renderer.material != initialMat){
+			renderer.material = initialMat;
+			
+		}
+	}
+	public void ApplyMat(){
+		renderer.material = transparentMat;
+	}
+}
