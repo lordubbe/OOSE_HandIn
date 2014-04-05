0d5876ae4d25991e006384a4952e7cf480f0da33
Jacob Lynggaard Olsen <jacoblynggaardolsen@hotmail.com>
2014-04-05 14:12:11 +0200

    Added taller walls + roof and some cool particles for torches

---
+++ Assets/DisplayOnlyCloseLightSources.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class DisplayOnlyCloseLightSources : MonoBehaviour {
+	public List<Transform> spotlights;
+	private bool ready;
+	public int distance;
+	// Update is called once per frame
+	void Start () {
+		ready=false;
+		StartCoroutine(waitAndLoad(2.0f));
+	}
+
+	void Update () {
+		if(ready){
+			print (Vector3.Distance(spotlights[0].transform.position, transform.position));
+			foreach (Transform spot in spotlights)
+			{
+				if (Vector3.Distance(spot.transform.position, transform.position) > distance){
+					spot.gameObject.SetActive(false);
+					print ("deactivating light");
+				}
+				else{spot.gameObject.SetActive(true); print ("light is on!");}
+			}
+		}
+	}
+
+	public IEnumerator waitAndLoad(float waitTime){
+		yield return new WaitForSeconds(waitTime);
+		spotlights = GameObject.Find("levelSpawner").GetComponent<LevelSpawn>().lights;
+		ready = true;
+	}
+	
+}
---
+++ Assets/Jacob/RandomizeLightIntensity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeLightIntensity : MonoBehaviour {
+
+	public float minIntensity = 0.5f;
+	public float maxIntensity = 1f;
+	
+	float random;
+	
+	void Start(){
+		random = Random.Range(0.0f, 635.0f);
+	}
+	
+	void Update(){
+		float noise = Mathf.PerlinNoise(random, Time.time);
+		light.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+	}
+
+}
