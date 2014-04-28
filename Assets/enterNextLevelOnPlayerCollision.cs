7741820b9858c14bd584c570fc315b2057a005cf
Jacob Lynggaard Olsen <jacoblynggaardolsen@hotmail.com>
2014-04-28 12:56:29 +0200

    Revert 4750bcf..75f989c
    This rolls back to commit 4750bcf671afed6a60ebffeed12b55347eae2e1d.

---
+++ Assets/enterNextLevelOnPlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enterNextLevelOnPlayerCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider){
+
+		if(collider.gameObject.tag == "Player"){
+			print ("restarting level");
+			Application.LoadLevel(Application.loadedLevel);
+			Destroy (this.gameObject);
+		}
+	}
+
+}
