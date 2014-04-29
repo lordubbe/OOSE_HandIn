f1b096b2a07d5ebadf087118f30565c62f24df22
Jacob Lynggaard Olsen <jacoblynggaardolsen@hotmail.com>
2014-04-29 15:11:11 +0200

    Improved menu and added functionality

---
+++ Assets/ExitApplicationOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitApplicationOnClick : MonoBehaviour {
+
+	void OnMouseUpAsButton() {
+		print ("Quitting Application!");
+		Application.Quit();
+	}
+}
