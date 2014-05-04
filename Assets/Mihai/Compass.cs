9c4c22c10a3b5f5225bd0bf248b3679015f954cc
mihai9323 <mihai9323@gmail.com>
2014-05-04 15:29:46 +0200

    start working on a compass

---
+++ Assets/Mihai/Compass.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compass : MonoBehaviour {
+    Transform playerCamera;
+
+	private void Update(){
+        transform.rotation = Quaternion.Euler(0, 0, playerCamera.rotation.eulerAngles.y);
+    }
+}
